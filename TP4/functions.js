707ccfe4debf569dd5c4bb711cc63d24a9e68106
Sofia Marzari <sofiamarzari3544@gmail.com>
2024-11-21 14:36:36 -0300

    TP4 - seccion video con animacion e iframe, bordes en numbers colors

--- TP4/functions.js
+++ TP4/functions.js
@@ -1,31 +1,54 @@
 let img_galeria_dinamica = document.getElementById('img_galeria_dinamica');
+let img_galeria_amigos = document.getElementById('img_galeria_amigos');
+let container_galeria_lateral = document.getElementById('container_galeria_lateral');
 let cont_galeria = 0;
 let paths_imagenes_galeria = ["uploads/mas_divertida_marco_1.png", "uploads/mas_divertida_marco_2.png", "uploads/mas_divertida_marco_3.png"];
+let paths_imagenes_galeria_lateral = ["uploads/rojo_uno.png", "uploads/naranja_dos.png", "uploads/3.png", "uploads/verde_cuatro.png", "uploads/azul_cinco.png", "uploads/violeta_6.png", "uploads/7.png", "uploads/rosa_ocho.png", "uploads/gris_nueve.png", "uploads/blanco_diez.png"];
 let btn_hamburguesa = document.getElementById('btn-nav-hamburguesa');
 let open_nav = false;
+
+let iframe_home_video = document.getElementById('iframe-video-home');
+let imagen_home_video = document.getElementById('imagen_home_video');
+let video_home_marco = document.getElementById('video_home_marco');
+let personaje_video = document.querySelector(".img_numero_video_seccion");
 $(document).ready(function(){
     let logo_header = document.getElementById('logo_header');
     logo_header.classList.add("no-visible");
 
-    /**************************Punto 8*************************************/
+    /**************************Punto 12*************************************/
     let numbers_colors_all = document.querySelectorAll(".img-number-color");
     numbers_colors_all.forEach((number) => {
         number.addEventListener("mouseover", function(ev){/*Mouse entra al elemento (sobre) */
             ev.preventDefault();
             this.style.transform = "scale(1.2)";
+            this.parentElement.style.boxShadow = "1px 1px 70px 1px #fbf9edd1 inset";
         });
     });
     numbers_colors_all.forEach((number) => {
         number.addEventListener("mouseout", function(ev){/*Mouse sale del elemento */
             ev.preventDefault();
             this.style.transform = "scale(1)";
+            this.parentElement.style.boxShadow = "1px 1px 70px 1px #3f3f3f inset";
         });
     });
-    /**************************fin - Punto 8*************************************/
+    /**************************fin - Punto 12*************************************/
     /**************************Punto 6*************************************/
     img_galeria_dinamica.src = paths_imagenes_galeria[0];
     setInterval(galeria_dinamica, 3000);
     /**************************fin - Punto 6*************************************/
+    container_galeria_lateral.innerHTML = '<img id="img_galeria_amigos" src="'+paths_imagenes_galeria_lateral[0]+'" alt="">';
+    /**************************Punto 10A********************************/
+    let iframe_home_video = document.getElementById('iframe-video-home');
+    iframe_home_video.classList.add('no-visible');
+    let btn_play_video_home = document.getElementById('btn-play-video-home');
+    btn_play_video_home.addEventListener('click', function(event){
+        iframe_home_video.classList.remove('no-visible');
+        iframe_home_video.classList.add('visible');
+        imagen_home_video.classList.add('no-visible');
+        iframe_home_video.setAttribute('src', iframe_home_video.getAttribute('src')+'?autoplay=1');
+        this.classList.add('no-visible');
+    });
+   /**************************fin - Punto 10A********************************/
 });
 /**************************Punto 6*************************************/
 function galeria_dinamica(){
@@ -119,14 +142,73 @@ document.addEventListener('scroll', function(ev){
     /**************************fin - Punto 5*************************************/
     /**************************Punto 7*************************************/
     let cards = document.querySelectorAll(".container_card");
-    if(window.scrollY >= 1700){
-        for(let i = 0; i <= 3; i++){
+    if(window.scrollY >= 1600){
+        for(let i = 0; i < 3; i++){
             cards[i].classList.add("container_card_animate"+(i+1));
         }
     }else{
-        for(let i = 0; i <= 3; i++){
+        for(let i = 0; i < 3; i++){
             cards[i].classList.remove("container_card_animate"+(i+1));
         }
     }
     /**************************fin - Punto 7*************************************/
+    /**************************Punto 9*************************************/
+    let container_galeria_lateral = document.getElementById('container_galeria_lateral');
+    if(window.scrollY >= 4300){
+       container_galeria_lateral.innerHTML = '<img id="img_galeria_amigos" src="'+paths_imagenes_galeria_lateral[0]+'" alt="">';
+       //container_galeria_lateral.children[0].classList.add("animate_general_lateral");
+    }
+    if(window.scrollY >= 4560){
+        container_galeria_lateral.innerHTML = '<img id="img_galeria_amigos" src="'+paths_imagenes_galeria_lateral[1]+'" alt="">';
+        //container_galeria_lateral.children[0].classList.add("animate_general_lateral");
+    }
+    if(window.scrollY >= 5140){
+        container_galeria_lateral.innerHTML = '<img style="width:40%;" id="img_galeria_amigos" src="'+paths_imagenes_galeria_lateral[2]+'" alt="">';
+        //container_galeria_lateral.children[0].classList.add("animate_general_lateral");
+    }
+    if(window.scrollY >= 5490){
+        container_galeria_lateral.innerHTML = '<img id="img_galeria_amigos" src="'+paths_imagenes_galeria_lateral[3]+'" alt="">';
+        //container_galeria_lateral.children[0].classList.add("animate_general_lateral");
+    }
+    if(window.scrollY >= 6290){
+        container_galeria_lateral.innerHTML = '<img id="img_galeria_amigos" src="'+paths_imagenes_galeria_lateral[4]+'" alt="">';
+        //container_galeria_lateral.children[0].classList.add("animate_general_lateral");
+    }
+    if(window.scrollY >= 6490){
+        container_galeria_lateral.innerHTML = '<img id="img_galeria_amigos" src="'+paths_imagenes_galeria_lateral[5]+'" alt="">';
+        //container_galeria_lateral.children[0].classList.add("animate_general_lateral");
+    }
+    if(window.scrollY >= 6990){
+        container_galeria_lateral.innerHTML = '<img style="width:30%;" id="img_galeria_amigos" src="'+paths_imagenes_galeria_lateral[6]+'" alt="">';
+        //container_galeria_lateral.children[0].classList.add("animate_general_lateral");
+    }
+    if(window.scrollY >= 7490){
+        container_galeria_lateral.innerHTML = '<img id="img_galeria_amigos" src="'+paths_imagenes_galeria_lateral[7]+'" alt="">';
+        //container_galeria_lateral.children[0].classList.add("animate_general_lateral");
+    }
+    if(window.scrollY >= 7990){
+        container_galeria_lateral.innerHTML = '<img id="img_galeria_amigos" src="'+paths_imagenes_galeria_lateral[8]+'" alt="">';
+        //container_galeria_lateral.children[0].classList.add("animate_general_lateral");
+    }
+    if(window.scrollY >= 8490){
+        container_galeria_lateral.innerHTML = '<img id="img_galeria_amigos" src="'+paths_imagenes_galeria_lateral[9]+'" alt="">';
+        //container_galeria_lateral.children[0].classList.add("animate_general_lateral");
+    }
+    /**************************fin - Punto 9*************************************/
+    /**************************Punto 10B********************************/
+    if(window.scrollY <= 10100){
+        video_home_marco.style.transform = "translateX(-150px)";
+        personaje_video.style.transform = "translateY(450px)";
+    }
+    if(window.scrollY >= 10100){
+        video_home_marco.style.transform = "translateX(0px)";
+        personaje_video.style.transform = "translateY(0px)";
+    }
+    if(window.scrollY >= 11330){
+        video_home_marco.style.transform = "translateX(-150px)";
+    }
+    if(window.scrollY >= 11840){
+        personaje_video.style.transform = "translateY(450px)";
+    }
+    /**************************fin - Punto 10B********************************/
 });
